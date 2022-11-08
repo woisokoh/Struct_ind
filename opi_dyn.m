0a110c8c1191c9a3f019e6d48f3313e6a831b1f3
Denis Patterson <patterd2@users.noreply.github.com>
2022-11-08 13:16:43 -0500

    Update parameters
    Rename and recalibrate parameters

--- opi_dyn.m
+++ opi_dyn.m
@@ -1,4 +1,4 @@
-function [x,deltas] = opi_dyn(maxt,Na,W1,W2,u,tau,d,b,beta,x0,delta0,sigma)
+function [x,deltas] = opi_dyn(maxt,Na,W1,W2,u,tau,d,b,alpha,x0,delta0,sigma)
 
 deltas=cell([1,maxt+1]);
 x=zeros(Na,maxt+1);
@@ -10,7 +10,7 @@ for t=1:maxt
     % dx/dt
     for i = 1:Na
         DaDo = 0;
-        SaDo = beta(i,1) * x(i,t);
+        SaDo = alpha(i,1) * x(i,t);
         for k = 1:Na
             if k ~= i
                 del = deltas{1,t};
--- run_opinion_model.m
+++ run_opinion_model.m
@@ -1,18 +1,19 @@
 %% set parameters
-maxt = 500;
-Na = 3;
+maxt = 10000;
+Na = 25;
 W1 = 1;
 W2 = 1;
 u = 1;
-tau = 1000;
-d = ones(Na,1);
-b = ones(Na,1);
-beta = ones(Na,1);
-sigma = 0.1;
-x0 = rand(Na,1);
-delta0 = ones(Na,Na) - eye(Na);
+tau = 1000; % timescale separation parameter
+d = 1.5*ones(Na,1); % attention span/decay of belief
+b = 0*ones(Na,1); % exogeneous forcing/influences
+alpha = 2*ones(Na,1); % strength of self-reinforcement
+sigma = 0.1; % noise parameter
+x0 = randn(Na,1);
+delta0 =  -0.05*(ones(Na,Na) - eye(Na));% - [0,0,0;0,0,0;0,0,0];zeros(Na,Na);
 %% Run the model
-x = opi_dyn(maxt,Na,W1,W2,u,tau,d,b,beta,x0,delta0,sigma);
+x = opi_dyn(maxt,Na,W1,W2,u,tau,d,b,alpha,x0,delta0,sigma);
 %% Plot outputs
 figure;
 plot(x','LineWidth',2);
+ylim([-2 2]);
