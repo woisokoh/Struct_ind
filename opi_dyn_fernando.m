e22f5cc8bac94a6ea34306d5b24f98ae346d3f81
Woi Sok Oh <w.oh@princeton.edu>
2023-02-16 22:06:51 -0500

    Added new linkage generation

--- opi_dyn_fernando.m
+++ opi_dyn_fernando.m
@@ -1,43 +1,68 @@
-function [x,deltas] = opi_dyn_fernando(maxt,Na,x0,gamma,eta,sigma)
+function [x,deltas] = opi_dyn_fernando(maxt,Na,x0,gamma,eta,sigma,W1,u,b,alpha,delta0)
 
 deltas=cell([1,maxt+1]);
 x=zeros(Na,maxt+1);
-deltas(1,1) = {delta0};
 x(:,1) = x0;
 dBt = randn(Na,maxt);
+deltas(1,1) = {delta0};
 
 for t=1:maxt
     rand_ord = randperm(Na);
-    temp_delta = deltas{1,t};
+%     temp_delta = deltas{1,t};
+    
+    % calculate similarity
+    Nij = (temp_delta * temp_delta.*(1-eye(Na))).^eta;
+    sum_Nij = sum(Nij')';
+    sij = Nij ./ sum_Nij;
+
     for j = 1:Na 
         % update network of agent rand_ord(i)
         % break an existing link and % add a new link
         if sum(temp_delta(rand_ord(j),:))>2
             break_indices = find(temp_delta(rand_ord(j),:)>0);
             break_link = break_indices(randperm(length(break_indices),1));
+            
+            % add a link - choose unconnected nodes & randomly choose 
+            % based on similarity degrees
+            add_indices = find(temp_delta(rand_ord(j),:)==0);
+            add_indices = add_indices(add_indices ~= rand_ord(j));
+            add_sij = sij(rand_ord(j),add_indices)/sum(sij(rand_ord(j),add_indices));
+            cum_prob = [0,cumsum(add_sij)];
+            r = rand;
+            add_link = add_indices(find(r>cum_prob,1,'last'));
+            
+            % update adjacency matrix
             temp_delta(rand_ord(j), break_link) = 0;
             temp_delta(break_link,rand_ord(j)) = 0;
-            deltas(1,t+1)= {temp_delta};
-            
-            add_indices = find(temp_delta(rand_ord(j),:)==0);
-            add_link = break_indices(randperm(length(add_indices),1));
             temp_delta(rand_ord(j), add_link) = 1;
-            temp_delta(break_link,rand_ord(j)) = 1;
+            temp_delta(add_link,rand_ord(j)) = 1;
+
             deltas(1,t+1)= {temp_delta};
         end
-        
-        
-        
-        % x_n+1 = f(x_n)
+    
+        DaDo = 0;
+        SaDo = alpha(j,1) * x(j,t);
+        for k = 1:Na
+            if k ~= j
+                del = temp_delta;
+                DaDo = DaDo + del(j,k)*x(k,t);
+            end
+        end
+        y = SaDo + DaDo;
         S = tanh(W1 * y);
-        x(j,t+1) = gamma*x(j,t) +  ...
-            + sigma*sqrt(dt)*dBt(j,t);
+        x(j,t+1) = x(j,t) + 1 * (-gamma * x(j,t) + b(j,1) + u * S) ...
+            + sigma*sqrt(1)*dBt(j,t);
+        
+
+%         x(j,t+1) = gamma*x(j,t) +  ...
+%             + sigma*sqrt(dt)*dBt(j,t);
+
     end
     
     %% ddelta/dt
-         fx = tanh(W2*abs(x(:,t) - x(:,t)'));
+%          fx = tanh(W2*abs(x(:,t) - x(:,t)'));
          %keyboard;
-         deltas(1,t+1) = {deltas{1,t} + dt * (-deltas{1,t}/tau + fx/tau)};
+%          deltas(1,t+1) = {deltas{1,t} + dt * (-deltas{1,t}/tau + fx/tau)};
     %deltas(1,t+1) = {delta0};
 end
 
--- run_opinion_model_fernando.m
+++ run_opinion_model_fernando.m
@@ -1,5 +1,5 @@
 %% set parameters
-maxt = 10000;
+maxt = 500;
 Na = 25;
 W1 = 1;
 W2 = 1;
@@ -10,16 +10,21 @@ b = 0*ones(Na,1); % exogeneous forcing/influences
 alpha = 0.01*ones(Na,1); % strength of self-reinforcement
 sigma = 0.0; % noise parameter
 x0 = randn(Na,1);
-tempDelta = randn(Na,Na);
-delta0 =  tempDelta.*(ones(Na) - eye(Na));%0.05*(ones(Na,Na) - eye(Na));% - [0,0,0;0,0,0;0,0,0];zeros(Na,Na);
+eta = 2;
+gamma = 0.5;
+% tempDelta = randn(Na,Na);
+% delta0 =  tempDelta.*(ones(Na) - eye(Na));%0.05*(ones(Na,Na) - eye(Na));% - [0,0,0;0,0,0;0,0,0];zeros(Na,Na);
+delta0 = full(adjacency(WattsStrogatz(Na,7,1))); % build a random small-world network for initial step.
 %% Run the model
-[x,deltas] = opi_dyn(maxt,Na,W1,W2,u,tau,d,b,alpha,x0,delta0,sigma);
+% [x,deltas] = opi_dyn(maxt,Na,W1,W2,u,tau,d,b,alpha,x0,delta0,sigma);
+[x,deltas] = opi_dyn_fernando(maxt,Na,x0,gamma,eta,sigma,W1,u,b,alpha,delta0);
+
 %% Plot outputs
 figure;
 plot(x','LineWidth',2);
 colormap jet; % colormap not working
 
-ylim([-2 2]);
+% ylim([-2 2]);
 
 legend;
 
@@ -35,3 +40,5 @@ imagesc(cell2mat(deltas(end)));
 colorbar;
 title('Connectivity Final State');
 
+figure;
+plot(graph(deltas{1,end}))
