ca467dc6c9ed8b3957d4682f56ce31a2cf53df96
Denis Patterson <patterd2@users.noreply.github.com>
2023-02-21 12:12:28 -0500

    Updates to Fernando's model (time stepping)
    Changes to the times stepping and saved data from interesting transient dynamics that leads to spontaneous consensus breaking.

--- run_opinion_model_fernando.m
+++ run_opinion_model_fernando.m
@@ -1,7 +1,7 @@
 %% set parameters
 maxt = 500;
-Na = 25;
-W1 = 1;
+Na = 300;
+W1 = 0.01;
 W2 = 1;
 u = 1;
 tau = 1; % timescale separation parameter
@@ -9,8 +9,8 @@ d = 1.5*ones(Na,1); % attention span/decay of belief
 b = 0*ones(Na,1); % exogeneous forcing/influences
 alpha = 0.01*ones(Na,1); % strength of self-reinforcement
 sigma = 0.0; % noise parameter
-x0 = randn(Na,1);
-eta = 2;
+x0 = 2*randn(Na,1);
+eta = 4;
 gamma = 0.5;
 % tempDelta = randn(Na,Na);
 % delta0 =  tempDelta.*(ones(Na) - eye(Na));%0.05*(ones(Na,Na) - eye(Na));% - [0,0,0;0,0,0;0,0,0];zeros(Na,Na);
@@ -26,7 +26,7 @@ colormap jet; % colormap not working
 
 % ylim([-2 2]);
 
-legend;
+%legend;
 
 %%
 
@@ -42,3 +42,18 @@ title('Connectivity Final State');
 
 figure;
 plot(graph(deltas{1,end}))
+title('Final Connectivity Graph');
+
+figure; 
+histogram(sum(cell2mat(deltas(1))));
+title('Initial Degree Distribution');
+
+figure; 
+histogram(sum(cell2mat(deltas(end))));
+title('Final Degree Distribution');
+
+%% unpack delta cell structure
+
+for i = 1:Na
+    
+end
